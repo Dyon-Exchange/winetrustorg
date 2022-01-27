41b39073acd6743d836b9a3e65a34597a21102e4
virlanchainworks <virlanchainworks@gmail.com>
2022-01-27 09:36:39 +0800

    initial save

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
-function getAssetByName(_value) {
-    const stringQuery = document.getElementById("assetsearchbyname").value;
-    alert(stringQuery)
+function getProductByName() {
+    const stringQuery = document.getElementById("productsearchbyname").value;
+    fetch(`http://127.0.0.1:3030/public/assets/product/${stringQuery}`)
+    .then(data => {
+        //alert(JSON.stringify(data))
+        console.log(data.json());
+    })
+    
 }
 
-function getAssetByTokenId(_value) {
+function getAssetByTokenId() {
     const stringQuery = document.getElementById("assetsearchbyid").value;
-    alert(stringQuery)
+    fetch(`http://127.0.0.1:3030/public/assets/token/${stringQuery}`)
+    .then(data => {
+        //alert(JSON.stringify(data))
+        console.log(data.json());
+    })
+   
 }
+
