78c58b1750d22f25a91ae2c9a9116ec081085309
virlanchainworks <virlanchainworks@gmail.com>
2022-01-27 08:37:30 +0800

    initial save

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function getAssetByName(_value) {
+    const stringQuery = document.getElementById("assetsearchbyname").value;
+    alert(stringQuery)
+}
+
+function getAssetByTokenId(_value) {
+    const stringQuery = document.getElementById("assetsearchbyid").value;
+    alert(stringQuery)
+}
