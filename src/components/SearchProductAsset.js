1e1611a7c15ae0a2b8726443804d039be76b83c3
virlanchainworks <virlanchainworks@gmail.com>
2022-02-11 13:49:14 +0800

    saving SearchProductAsset table using @mui

--- src/components/SearchProductAsset.js
+++ src/components/SearchProductAsset.js
@@ -1,6 +1,7 @@
 import React, { createContext, useCallback, useEffect, useState, useMemo, ReactNode } from "react";
 import { Button } from '@material-ui/core';
 import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper} from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
 import { extendTheme } from '@chakra-ui/react';
 import axios from 'axios';
 
@@ -9,15 +10,25 @@ const SearchProductAsset = () => {
     let [srchstrg, setsrchstrg] = useState();
     let [rowdata, setrowdata] = useState([]);
 
+    const columns = [
+        {field: 'id',headerName: 'ID', width: 5 },
+        {field: 'assetname',headerName: 'Asset Name', width: 20 },
+        {field: 'description',headerName: 'Product Description', width: 20 },
+        {field: 'tokenid',headerName: 'Token ID', width: 20 },
+        {field: 'location',headerName: 'Location', width: 20 },
+        {field: 'owner',headerName: 'Owner', width: 20 }
+    ];
+
 
     function searchProduct() {
+        let rowCounter = 1;
         let queryStr = `https://dev.winetrust.org/db/public/assets/product/${srchstrg}`;
         axios.get(queryStr)
         .then(result => {
             result.data.forEach(prod => {
-                console.log(`${prod.product.longName} ${prod.product.description}`);
+                //console.log(`${prod.product.longName} ${prod.product.description}`);
                 setrowdata(
-                        rowdata => [...rowdata, prod.product]
+                        rowdata => [...rowdata, {id: rowCounter++, assetname:prod.product.longName, description:prod.product.description, tokenid: 123, location:"United States", owner: "Jayper Sanchez"}]
                 )
             })
             
@@ -35,19 +46,14 @@ const SearchProductAsset = () => {
                 <input type="search" onChange={getSrchStr} placeholder="Enter Partial or whole Product Name" />
                 <Button onClick={searchProduct}>Search</Button> 
             </div>
-            <div className="search-result-pane">
-                    <table>
-                    <tr>
-                        <th>Asset Name</th>
-                        <th>Description</th>
-                        <th>NFT Token ID</th>
-                        <th>Location</th>
-                        <th>Owner</th>
-                    </tr>
-                    {
-                        rowdata    
-                    }
-                   </table>
+            <div style={{ height: 400, width: '100%' }}>
+                <DataGrid
+                    rows={rowdata}
+                    columns={columns}
+                    pageSize={5}
+                    rowsPerPageOptions={[5]}
+                    checkboxSelection
+                />
             </div>
         </div>
         
