a9f799aad99372d225d669169730fec50f0c9254
Anju <anju.joshi@cruworldwine.com>
2022-02-22 12:15:13 +0530

    created asset listing

---
+++ src/components/atoms/tables/StyledDataGrid.ts
@@ -0,0 +1,28 @@
+import { styled } from '@material-ui/styles'
+import { DataGrid } from '@mui/x-data-grid'
+
+// min height and max height takes into account the top bar
+
+const StyledDataGrid = styled(DataGrid)({
+  border: 'none',
+  minHeight: '225px',
+  height: 'calc(100vh - 300px)',
+  '& .MuiDataGrid-columnHeaders': {
+    background: '#e2e8f0'
+  },
+  '& .MuiDataGrid-cell': {
+    'font-size': '13px'
+  },
+  '& .MuiDataGrid-iconButtonContainer, .MuiDataGrid-menuIcon': {
+    display: 'none'
+  },
+  '& .MuiDataGrid-columnHeader:hover .MuiDataGrid-iconButtonContainer, .MuiDataGrid-columnHeader:hover .MuiDataGrid-menuIcon':
+    {
+      display: 'block'
+    },
+  '& .MuiDataGrid-columnSeparator': {
+    visibility: 'hidden'
+  }
+})
+
+export default StyledDataGrid
