f4d2a57f9eb6f432cd8afe3a92aa29051a9cfeee
Entangle Software <entanglesoftware@gmail.com>
2022-03-01 12:14:38 +0530

    Added basic connect to metamask

--- src/components/ConnectMetaMask.tsx
+++ src/components/ConnectMetaMask.tsx
@@ -1,25 +1,10 @@
 import React, { useEffect } from 'react'
-import { Button } from '@material-ui/core'
-
-//export const WalletContext = React.createContext();
+import BootstrapBlueBtn from './atoms/buttons/BootStrapBlueBtn'
 
 const ConnectMetaMask = () => {
-
-  /*interface IWalletContext {
-    provider: undefined;
-    isMetaMaskInstalled: boolean | undefined;
-    walletConnected: boolean;
-    networkDetails: undefined;
-    isConnected: boolean;
-  }*/
-
-  //const [provider, setProvider] = useState();
-  //const [IsMetamaskInstalled, setIsMetamaskInstalled] = useState(false);
-  //const [walletaddress, setWalletAddress] = useState();
-
   //Rinkeby and Ethereum only
-  const supportNetwork = ['0x4','0x1'] 
-  
+  const supportNetwork = ['0x4', '0x1']
+
   useEffect(() => {
     if (window.ethereum !== undefined) {
       window.ethereum.on('chainChanged', () => {
@@ -29,27 +14,33 @@ const ConnectMetaMask = () => {
   })
 
   async function connectMetaMask() {
+    if (window.ethereum !== undefined) {
+      alert('Please install metamask')
+    }
+
     const chainId = await window.ethereum.request({ method: 'eth_chainId' })
     const accounts = await window.ethereum.request({
       method: 'eth_requestAccounts'
     })
-    //const selectedAddress = await window.ethereum.request({method: 'eth_selectedAddress'});
+    console.log(supportNetwork)
     console.log(chainId)
-    //check if connected to the supported network
     if (supportNetwork.includes(chainId)) {
-      //setIsMetamaskInstalled(false);
       alert('Please connect to either Ethereum or Rinkeby')
     } else {
-      //setIsMetamaskInstalled(true);
-      //setWalletAddress(accounts[0]);
       console.log(`Connected with ${accounts[0]}`)
     }
   }
 
   return (
-    <div className="content">
-      <Button onClick={connectMetaMask}>Connect to Metamask</Button>
-    </div>
+    <BootstrapBlueBtn
+      variant="contained"
+      disableRipple
+      size="medium"
+      style={{ borderRadius: '5px' }}
+      sx={{ p: '6px 25px!important' }}
+      onClick={connectMetaMask}>
+      Connect
+    </BootstrapBlueBtn>
   )
 }
 
