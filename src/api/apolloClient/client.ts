aace5ea616f4bf1b94aa85b8e0c361d508214128
Entangle Software Tech Team <tech@entanglesoftware.com>
2022-04-26 19:06:29 +0530

    Revert "Dummy commitjJ"
    This reverts commit 47d93033b4fa3217652daf65354ad97d14e844fb.

--- src/api/apolloClient/client.ts
+++ src/api/apolloClient/client.ts
@@ -4,8 +4,6 @@ const apolloUrl =
   process.env.REACT_APP_GRAPHQL_API ??
   'https://api.thegraph.com/subgraphs/name/dyon-exchange/winetrust-rinkeby'
 
-console.log(apolloUrl)
-
 export const apolloClient = new ApolloClient({
   uri: apolloUrl,
   cache: new InMemoryCache()
