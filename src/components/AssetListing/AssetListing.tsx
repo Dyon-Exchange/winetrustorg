d0e4b66b43cdf78e292e4349fd2efe5716cf34e5
Entangle Software Tech Team <tech@entanglesoftware.com>
2022-05-05 15:06:25 +0530

    fixed the issue

--- src/components/AssetListing/AssetListing.tsx
+++ src/components/AssetListing/AssetListing.tsx
@@ -44,6 +44,8 @@ const AssetListing = () => {
     navigate(`/asset-home/${param}`)
   }
 
+  console.log(classes)
+
   return (
     <div className={classes.root}>
       <Container className="container">
