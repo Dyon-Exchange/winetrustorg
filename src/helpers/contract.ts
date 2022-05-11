8b5105e221ed75fb9a708c400b8af6fa659c0ba5
chychkoi <chychkoi@gmail.com>
2022-05-11 15:59:04 +0300

    Implement redemption feature

---
+++ src/helpers/contract.ts
@@ -0,0 +1,13 @@
+import Web3 from "web3";
+import { AbiItem } from "web3-utils";
+import WineTrustTokenJson from './abis/WineTrustToken.json';
+
+export const web3 = new Web3(Web3.givenProvider);
+
+const tokenAddress = process.env.REACT_APP_WINETRUST_CONTRACT_URL;
+const tokenContract = new web3.eth.Contract(WineTrustTokenJson.abi as AbiItem[], tokenAddress);
+
+export const WineTrustToken = {
+  address: tokenAddress,
+  contract: tokenContract,
+};
--- src/interfaces/Portfolio.ts
+++ src/interfaces/Portfolio.ts
@@ -1,11 +1,13 @@
+export interface WineTrusttoken {
+  id: string
+  identifier: string
+  uri: string
+}
+
 export interface WineTrustbalance {
   id: string
   valueExact: string
-  token: {
-    id: string
-    identifier: string
-    uri: string
-  }
+  token: WineTrusttoken
 }
 
 export interface AccountPortfolio {
@@ -19,4 +21,21 @@ export interface AccountPortfolioResponse {
 
 export interface AccountPortfolioRequest {
   accountId: string
+}
+
+export interface WineTrusttokenMetadataAttribute {
+  trait_type: string
+  value: any
+}
+
+export interface WineTrusttokenMetadata {
+  name: string
+  description: string
+  image: string
+  external_url: string
+  attributes: WineTrusttokenMetadataAttribute[]
+}
+
+export interface PortfolioRowData extends WineTrusttoken {
+  metadata: WineTrusttokenMetadata
 }
